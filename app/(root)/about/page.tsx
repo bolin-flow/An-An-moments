9da2d31a0ad76bc5c79f1c0b97be84aeb66e4117
Bolin <libolin11203203@gmail.com>
2026-01-07 14:14:45 -0500

    page is shell and wrap dynamic component

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => {
   // Uncomment this line to trigger the error
-  throw new Error("This is a test error for the Error Boundary!");
+  // throw new Error("This is a test error for the Error Boundary!");
 
   return <div>About</div>;
 };
